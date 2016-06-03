21d310e40bb5f70c03fd3a226c69ffef5fbf6aef
vivek <typedefstructer@gmail.com>
2016-06-03 19:31:41 +0530

    mail settings

---
+++ plugins.sh
@@ -0,0 +1,10 @@
+pushd plugins/
+git clone --depth=1 https://github.com/angusmcleod/discourse-ratings.git
+git clone --depth=1 https://github.com/gdpelican/babble.git
+git clone --depth=1 https://github.com/angusmcleod/discourse-topic-previews.git
+git clone --depth=1 https://github.com/discourse/discourse-cakeday.git
+git clone --depth=1 https://github.com/discoursehosting/discourse-adsense.git
+git clone --depth=1 https://github.com/matthieu-lapeyre/wp-discourse-topic-integration.git
+git clone --depth=1 https://github.com/cpradio/discourse-plugin-poll-ui.git
+git clone --depth=1 https://github.com/rux-pizza/discourse-awesome-bbcodes.git
+popd
